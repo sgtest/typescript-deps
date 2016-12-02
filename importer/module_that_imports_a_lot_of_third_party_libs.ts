27c6f505001abc5199f861f662dc54ee4b9bc5a7
Beyang Liu <beyang@sourcegraph.com>
2016-12-01 18:03:29 -0800

    add Angular TypeScript dependency

--- importer/module_that_imports_a_lot_of_third_party_libs.ts
+++ importer/module_that_imports_a_lot_of_third_party_libs.ts
@@ -1,10 +1,17 @@
 import * as map from "lodash/map";
+import { Component } from "@angular/core";
 
 console.log("Hello, world");
-doSomething();
+doSomethingWithLodash();
 
-export function doSomething(): void {
+export function doSomethingWithLodash(): void {
 	const l = [1, 2, 3];
 	const l2 = map(l, (e) => "" + e);
 	console.log(l2);
 }
+
+@Component({
+	selector: 'my-app',
+	template: `<h1>Hello {{name}}</h1>`
+})
+export class AppComponent { name = 'Angular'; }
